ed61db48b746ad94adc726599178daa94ad6b445
Amit Tiwari <amit067843@gmail.com>
2025-12-30 16:45:14 +0530

    index pages for upto patients report submenu created

--- js/core/sidebar.js
+++ js/core/sidebar.js
@@ -1,4 +1,5 @@
 let sidebarCollapsed = false;
+let activeSubMenuId = null;
 
 function toggleSidebar() {
   const sidebar = document.getElementById("sidebar");
@@ -13,28 +14,44 @@ function toggleSidebar() {
     const logo = document.getElementById("logo");
     const sidebarTexts = document.querySelectorAll(".sidebar-text");
     const sidebarSubmenus = document.querySelectorAll(".sidebar-submenu");
+    const sidebarSubmenuIcons = document.querySelectorAll(
+      ".sidebar-submenu-icon"
+    );
 
     sidebarCollapsed = !sidebarCollapsed;
 
     if (sidebarCollapsed) {
+      //collapse sidebar
       sidebar.classList.remove("w-64");
       sidebar.classList.add("w-20");
       searchBar.classList.add("hidden");
       logo.classList.add("hidden");
       sidebarTexts.forEach((text) => text.classList.add("hidden"));
       sidebarSubmenus.forEach((submenu) => submenu.classList.add("hidden"));
+      if (activeSubMenuId) {
+        sidebarSubmenuIcons.forEach((icon) => {
+          icon.classList.remove("rotate-90");
+        });
+      }
     } else {
+      //expand sidebar
       sidebar.classList.remove("w-20");
       sidebar.classList.add("w-64");
       searchBar.classList.remove("hidden");
       logo.classList.remove("hidden");
       sidebarTexts.forEach((text) => text.classList.remove("hidden"));
+
+      if (activeSubMenuId) {
+        toggleSubmenu(activeSubMenuId);
+      }
     }
   }
 }
 
 function handleMenuClick(id) {
   const isLargeScreen = window.innerWidth >= 1024;
+
+  activeSubMenuId == id ? (activeSubMenuId = null) : (activeSubMenuId = id);
 
   if (isLargeScreen && sidebarCollapsed) {
     toggleSidebar();
@@ -51,7 +68,7 @@ function toggleSubmenu(id) {
   const icon = document.getElementById(id + "-icon");
 
   submenu.classList.toggle("hidden");
-  icon.classList.toggle("rotate-180");
+  icon.classList.toggle("rotate-90");
 }
 
 export { toggleSidebar, handleMenuClick };
